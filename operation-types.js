70b81e545f0057eb7c1b5605e509a4d5fb629d25
Maiesha Moohan <maieshamoohan@gmail.com>
2026-05-20 11:48:37 +0200

    Add files via upload

---
+++ operation-types.js
@@ -0,0 +1,21 @@
+let number1 = 10;
+// let is used for variables that can change
+
+let number2 = 5;
+
+let number_1_As_String = number1.toString();
+// Converts the number in 'number1' (10) into a string ("10") and saves it in 'number_1_As_String'
+
+let sum = number1 + number2;
+// Adds 'number1' (10) and 'number2' (5) together, saving the result (15) in 'sum'
+
+let product = number1 * number2;
+
+console.log("Number 1 as a String:", number_1_As_String);
+// Prints the text "Number 1 as a String:" followed by the actual string value ("10") to the console
+
+console.log("Sum is:", sum);
+// Prints the text "Sum is:" followed by the calculated mathematical sum (15) to the console
+
+console.log("Product is:", product);
+// Prints the text "Product is:" followed by the calculated mathematical product (50) to the console
